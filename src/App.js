c5debd992e905518bf152de968bfb59aac6ece93
mohammedsaber <mohammedsaber43@gmail.com>
2021-08-16 00:39:53 +0200

    Finish Basic functionality + Async feature remains

--- src/App.js
+++ src/App.js
@@ -3,11 +3,52 @@ import React from 'react';
 import InfiniteScroll from './InfiniteScroll';
 
 class App extends React.Component {
+
+  state = {
+    isLoading: false,
+    count: 0,
+    data: [
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+    ]
+  }
+
+  onLimitReached = () => {
+    this.setState({ isLoading: true }, () => {
+      setTimeout(() => {
+        this.setState(prevState => ({
+          isLoading: false,
+          count: prevState.count + 1,
+          data: prevState.data.concat([
+            <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
+            <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
+          ]) 
+        }))
+      }, 2000)
+    });
+  }
+
   render() {
+    const { isLoading, data, count } = this.state;
+    console.log('count',count);
     return (
-      <div>
-        <InfiniteScroll />
-      </div>
+      <React.Fragment>
+        <InfiniteScroll 
+          height={500}
+          isLoading={isLoading}
+          isDataFinished={count === 2}
+          onLimitReached={this.onLimitReached}
+          scrollThreshold={.1}
+          render={() => data.map(item => item)}
+        />
+      </React.Fragment>
     );
   }
 }
--- src/InfiniteScroll.js
+++ src/InfiniteScroll.js
@@ -1,27 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const scrollableWrapperKey = 'infinite-scroll-wrapper';
 class InfiniteScroll extends React.Component {
 
   componentDidMount() {
-    // document.querySelector('#infinite-scroll-wrapper').addEventListener('scroll', this.onScroll);
-    this.scrollableWrapper.addEventListener('scroll', this.onScroll)
+    this.scrollableWrapper.addEventListener('scroll', this.onScroll);
+    // Todo
+    // async = {
+    //   endpoint: '',
+    //   totalCountProp: '',
+    //   dataTargetKey: 'items',
+    //   page: 0,
+    //   pageSize: 10, 
+    // }
+  }
+
+  componentWillUnmount() {
+    this.scrollableWrapper.removeEventListener('scroll', this.onScroll)
   }
 
   onScroll = e => {
-    console.log('scrollableWrapper', this.scrollableWrapper)
+    const { isLoading } = this;
+    const { onLimitReached, isDataFinished } = this.props;
     const { scrollHeight, scrollTop, clientHeight } = this.scrollableWrapper;
-    console.log('scrollHeight, scrollTop, clientHeight', scrollHeight, scrollTop, clientHeight)
     const offsetBottom = scrollHeight - (scrollTop + clientHeight);
-    console.log('offsetBottom', offsetBottom)
+
+    if(
+      (offsetBottom < this.scrollLimit) 
+      && 
+      !isLoading
+      &&
+      !isDataFinished
+    ) {
+      this.setState({ isLoading: true }, () => {
+        onLimitReached();
+      });
+    }
   }
 
   get scrollableWrapper() {
-    return document.getElementById('infinite-scroll-wrapper');
+    return document.getElementById(scrollableWrapperKey);
+  }
+
+  get scrollLimit() {
+    const { scrollThreshold } = this.props;
+    const { scrollHeight } = this.scrollableWrapper;
+
+    const ratio = scrollThreshold > 1 ? 1 : scrollThreshold
+
+    return ratio * scrollHeight;
+  }
+
+  get isLoading() {
+    const { isLoading } = this.props;
+
+    return isLoading;
   }
 
   render() {
-    const { height } = this.props;
+    const { isLoading } = this;
+    const { height, render, loader } = this.props;
 
     const wrapperStyle = {
       height,
@@ -29,47 +68,48 @@ class InfiniteScroll extends React.Component {
     };
 
     return(
-      <div style={wrapperStyle} id='infinite-scroll-wrapper'>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        {/* <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-        <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div> */}
+      <div style={wrapperStyle} id={scrollableWrapperKey}>
+        {render()}
+        {isLoading && loader}
       </div>
     );
   }
 }
 
 InfiniteScroll.propTypes = {
-  height: PropTypes.oneOfType([ PropTypes.number, PropTypes.string ])
+  height: PropTypes.oneOfType([ PropTypes.number, PropTypes.string ]),
+
+  scrollThreshold: PropTypes.number,
+
+  loader: PropTypes.any,
+
+  isLoading: PropTypes.bool,
+  isDataFinished: PropTypes.bool,
+  
+  render: PropTypes.func,
+  onLimitReached: PropTypes.func,
 };
 
 InfiniteScroll.defaultProps = {
-  height: 400
+  // height of the scrollable container by default will be 100vh
+  // you can change it to any value to control it
+  height: '100vh',
+  // It's used to calculate the distance in which you will fetch the data again using onLimitReached()
+  // and triger loading state to show loader
+  // 
+  scrollThreshold: 0.2,
+  // To show the loader when the limit reached while there is feaching in progress
+  isLoading: false,
+  // To control wheather there is a data to fetch or not 
+  // if true -----> onLimitReached() will not be called
+  isDataFinished: false,
+  // Loader indicator
+  loader: <h3 style={{color: 'red'}}>isLoading...</h3>,
+  // Alternative for children prop
+  render() {},
+  // will be fired when 
+  // isDataFinished = false and scrollOffsetBottom < calculated scrollLimit based on scrollThreshold
+  onLimitReached() {},
 };
 
 export default InfiniteScroll;
