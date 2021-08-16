dd0b886ed0b50b5e675ea3a6e175668e3f357dcb
mohammedsaber <mohammedsaber43@gmail.com>
2021-08-17 00:29:05 +0200

    Initiate Async Functionality + remaining ---->[ enhance the functionality + handle is finished logic + test with real data ]

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ class App extends React.Component {
   state = {
     isLoading: false,
     count: 0,
-    data: [
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-      <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-    ]
+    data: [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
   }
 
   onLimitReached = () => {
@@ -26,10 +16,7 @@ class App extends React.Component {
         this.setState(prevState => ({
           isLoading: false,
           count: prevState.count + 1,
-          data: prevState.data.concat([
-            <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>,
-            <div style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>1</div>
-          ]) 
+          data: prevState.data.concat([prevState.data.length + 1, prevState.data.length + 2])
         }))
       }, 2000)
     });
@@ -37,16 +24,28 @@ class App extends React.Component {
 
   render() {
     const { isLoading, data, count } = this.state;
-    console.log('count',count);
+
     return (
       <React.Fragment>
         <InfiniteScroll 
           height={500}
+          async={{ 
+            configs: {
+              url: 'https://pixabay.com/api/'
+            },
+            query: 'key=21237839-acf386e90dbcfa64cab0c353e',
+            dataTargetKey: 'hits',
+            pageSizeProp: 'per_page',
+            pageSize: 10,
+            page: 1
+           }}
           isLoading={isLoading}
-          isDataFinished={count === 2}
+          // isDataFinished={count === 2}
           onLimitReached={this.onLimitReached}
           scrollThreshold={.1}
-          render={() => data.map(item => item)}
+          render={() => data.map(item => (
+            <div key={item} style={{ height: 50, border: '1px solid red', marginBottom: 10 }}>{item}</div>
+          ))}
         />
       </React.Fragment>
     );
--- src/InfiniteScroll.js
+++ src/InfiniteScroll.js
@@ -1,19 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import axios from 'axios'
+
 const scrollableWrapperKey = 'infinite-scroll-wrapper';
 class InfiniteScroll extends React.Component {
 
+  state = {
+    page: this.asyncConfigs.page,
+    pageSize: this.asyncConfigs.pageSize,
+    isRequestLoading: false,
+    error: null
+  };
+
   componentDidMount() {
     this.scrollableWrapper.addEventListener('scroll', this.onScroll);
-    // Todo
-    // async = {
-    //   endpoint: '',
-    //   totalCountProp: '',
-    //   dataTargetKey: 'items',
-    //   page: 0,
-    //   pageSize: 10, 
-    // }
+
+    // Async handler
+    if(this.isAsync) {
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+    const { page, pageSize } = this.state;
+    const { debug } = this.props;
+    const { 
+      configs, 
+      dataTargetKey, 
+      query, 
+      pageProp,
+      pageSizeProp 
+    } = this.asyncConfigs;
+
+    this.setState({ isRequestLoading: true }, () => {
+      let url = `${configs.url}?${pageProp}=${page}&${pageSizeProp}=${pageSize}`;
+
+      if(query) {
+        url = `${url}&${query}`
+      }
+
+      axios({
+        ...configs,
+        url
+      })
+        .then(res => {
+          if(debug) {
+            console.warn('[Success Response]')
+            console.warn('You are targetting [', dataTargetKey, '] from', res.data,',if you want to target another prop use [ dataTargetKey ]')
+          }
+
+          const data = res.data[dataTargetKey];
+          this.setState({ data})
+        })
+        .catch(({ response }) => {
+          if(debug) {
+            console.error('[Err Response]')
+            console.error('Error is', response)
+          }
+
+          this.setState({ error: response })
+        })
+        .finally(() => this.setState({ isRequestLoading: false }))
+    });
   }
 
   componentWillUnmount() {
@@ -22,20 +71,29 @@ class InfiniteScroll extends React.Component {
 
   onScroll = e => {
     const { isLoading } = this;
+    const { error } = this.state;
     const { onLimitReached, isDataFinished } = this.props;
     const { scrollHeight, scrollTop, clientHeight } = this.scrollableWrapper;
     const offsetBottom = scrollHeight - (scrollTop + clientHeight);
-
+    console.log('isLoading', );
     if(
       (offsetBottom < this.scrollLimit) 
       && 
       !isLoading
       &&
       !isDataFinished
+      && 
+      // to prevent circular fetch in case of error
+      !error 
     ) {
-      this.setState({ isLoading: true }, () => {
-        onLimitReached();
-      });
+
+      if(!this.isAsync) {
+        return onLimitReached();
+      }
+
+      this.setState(prevState => ({
+        page: prevState.page + 1
+      }), () => this.fetchData())
     }
   }
 
@@ -53,19 +111,58 @@ class InfiniteScroll extends React.Component {
   }
 
   get isLoading() {
+    const { isAsync } = this;
     const { isLoading } = this.props;
+    const { isRequestLoading } = this.state;
 
-    return isLoading;
+    return isAsync ? isRequestLoading : isLoading;
+  }
+
+  get asyncConfigs() {
+    const { async } = this.props;
+
+    const enhancedConfigs = {
+      query: '',
+      pageProp: 'page',
+      dataTargetKey: 'items',
+      pageSizeProp: 'pageSize',
+      totalCountProp: 'totalCount',
+
+      page: 0,
+      pageSize: 40,
+      
+    };
+
+    return { 
+      ...enhancedConfigs, 
+      ...async,
+      configs: {
+        method: 'GET',
+        url: '', 
+        ...async.configs
+      },
+    };
+  }
+
+  get isAsync() {
+    const { configs } = this.asyncConfigs;
+
+    return !!configs.url;
   }
 
   render() {
     const { isLoading } = this;
-    const { height, render, loader } = this.props;
+    const { height, render, loader, debug } = this.props;
 
     const wrapperStyle = {
       height,
-      overflow: "scroll",
+      overflow: "auto",
     };
+    console.log('asyncConfigs', this.asyncConfigs, this.isAsync);
+    if(debug) {
+      console.warn('props', this.props);
+      console.warn('state', this.state);
+    }
 
     return(
       <div style={wrapperStyle} id={scrollableWrapperKey}>
@@ -78,6 +175,19 @@ class InfiniteScroll extends React.Component {
 
 InfiniteScroll.propTypes = {
   height: PropTypes.oneOfType([ PropTypes.number, PropTypes.string ]),
+
+  async: PropTypes.shape({
+    configs: PropTypes.object,
+
+    query: PropTypes.string,
+    pageProp: PropTypes.string,
+    pageSizeProp: PropTypes.string,
+    dataTargetKey: PropTypes.string,
+    totalCountProp: PropTypes.string,
+
+    page: PropTypes.number,
+    pageSize: PropTypes.number, 
+  }),
 
   scrollThreshold: PropTypes.number,
 
@@ -96,7 +206,6 @@ InfiniteScroll.defaultProps = {
   height: '100vh',
   // It's used to calculate the distance in which you will fetch the data again using onLimitReached()
   // and triger loading state to show loader
-  // 
   scrollThreshold: 0.2,
   // To show the loader when the limit reached while there is feaching in progress
   isLoading: false,
@@ -105,6 +214,33 @@ InfiniteScroll.defaultProps = {
   isDataFinished: false,
   // Loader indicator
   loader: <h3 style={{color: 'red'}}>isLoading...</h3>,
+  // used to log the state and props
+  debug: false,
+  // It used for advanced usage when ur data and pagination relies on the server 
+  // and you don't want to handle the updation everytime
+  async: {
+    // axios request configs like (method, headers, bearer, ...etc)
+    configs: {
+      method: 'GET',
+      // The endpoint you want to target
+      url: '',
+    },
+    // Key to target the data from response in order to iterate over
+    dataTargetKey: 'items',
+    // total items from server in order to calculate isDataFinished automatically 
+    totalCountProp: 'totalCount',
+    // to add custom query
+    query: '',
+    // initial request page, sometimes it starts from 0 or 1 
+    // it depends on your endpoint configuration
+    page: 0,
+    // number of rows you need per request or per page
+    pageSize: 40,
+    // page prop to send it as a query like, endpoint?page=3
+    pageProp: 'page',
+    // page size prop to send it as a query like, endpoint?page=3&pageSize=20
+    pageSizeProp: 'pageSize',
+  },
   // Alternative for children prop
   render() {},
   // will be fired when 
